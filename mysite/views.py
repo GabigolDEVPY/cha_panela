2d3795a2c2b883a6f979e73df760abd8004b2c45
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-02-02 20:30:27 -0300

    arrumando arquivos

---
+++ mysite/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+class Home(TemplateView):
+    template_name = "home/index.html"
+    
+    def get_queryset(self):
+        return super().get_queryset()
+    
