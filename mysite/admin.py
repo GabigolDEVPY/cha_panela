d9edf466e9795cdcbb58118872914766a8160181
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2026-02-02 21:37:19 -0300

    arrumando

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from . models import Items
 # Register your models here.
+
+@admin.register(Items)
+class ItemsPainel(admin.ModelAdmin):
+    list_display = ("name", "active")
+
--- mysite/views.py
+++ mysite/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from . models import Items
 from django.views.generic import TemplateView
 # Create your views here.
 
 class Home(TemplateView):
     template_name = "index.html"
     
-    def get_queryset(self):
-        return super().get_queryset()
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["items"] = Items.objects.filter(active=True)
+        return context
+    
     
